13b3d98adc4066429ec1126a74bd916880ebf62b
Chaidar <chaidarmerak1996@gmail.com>
2025-05-27 18:59:19 +0800

    AdminModel ditambahkan

--- app/Models/EventModel.php
+++ app/Models/EventModel.php
@@ -1,23 +1,24 @@
 <?php 
-    namespace App\Models;
-    use CodeIgniter\Model;
 
-    class EventModel extends Model{
-        protected $table = 'event';
-        protected $primaryKey = 'id_event';
-        protected $allowedFields = [
-            'id_admin', 
-            'judul_event', 
-            'tanggal_event', 
-            'lokasi_event', 
-            'harga_tiket', 
-            'kategori_tiket', 
-            'link_tiket', 
-            'deskripsi_event', 
-            'sponsor', 
-            'guest_star', 
-            'booth_list'
-        ];
-    }
+namespace App\Models;
+use CodeIgniter\Model;
+
+class EventModel extends Model {
+    protected $table = 'event';
+    protected $primaryKey = 'id_event';
+    protected $allowedFields = [
+        'id_admin', 
+        'judul_event', 
+        'tanggal_event', 
+        'lokasi_event', 
+        'harga_tiket', 
+        'kategori_tiket', 
+        'link_tiket', 
+        'deskripsi_event', 
+        'sponsor', 
+        'guest_star', 
+        'booth_list'
+    ];
+}
 
 ?>
