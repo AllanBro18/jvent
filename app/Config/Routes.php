17069e1285903bcf27c4928cbe28cc5c96dd22f5
Chaidar <chaidarmerak1996@gmail.com>
2025-05-27 19:46:31 +0800

    refactor(admin routes): Adding route for Admin login and Register

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,14 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'EventController::index');
 $routes->get('events', 'EventController::filter');
 $routes->get('detail', 'EventController::detail');
+
+
+// LOGIN for ADMIN
+$routes->get('login', 'Auth::index');
+$routes->get('/auth/login', 'Auth::login');
+$routes->get('/auth/logout', 'Auth::logout');
+
+$routes->get('/admin', 'Admin::index', ['filter' => 'authGuard']);
+
+$routes->get('/register', 'Auth::showRegister');
+$routes->get('/auth/register', 'Auth::register');
