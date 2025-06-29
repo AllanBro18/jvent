22335556abb36a5692178b880b46e984ecbcfc64
Chaidar <chaidarmerak1996@gmail.com>
2025-06-29 21:03:11 +0800

    final css

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <!-- tailwind -->
-    <link rel="stylesheet" href="<?= base_url('assets/css/styles.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/final.css') ?>">
 </head>
 <style>
     
